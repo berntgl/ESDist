0a0609d635a040d33eb0932a9b6fc322a35d7f99
Ben Glaser <bernt@glsr.nl>
2022-09-07 16:43:19 +0200

    Fixed issue with unreadable function input arguments

--- R/esd_plot.R
+++ R/esd_plot.R
@@ -1,9 +1,9 @@
 esd_plot <- function(df, cohensd) {
-  cdq25 <- quantile(cohensd, prob = 0.25)
-  cdq50 <- quantile(cohensd, prob = 0.50)
-  cdq75 <- quantile(cohensd, prob = 0.75)
+  cdq25 <- quantile({{ cohensd }}, prob = 0.25)
+  cdq50 <- quantile({{ cohensd }}, prob = 0.50)
+  cdq75 <- quantile({{ cohensd }}, prob = 0.75)
 
-  ggplot(data = df, aes(cohensd)) + geom_histogram(binwidth = .25) +
+  ggplot(data = df, aes({{ cohensd }})) + geom_histogram(binwidth = .25) +
     geom_vline(aes(xintercept = cdq25), color = "red", linetype = "dashed", size = 1) +
     geom_vline(aes(xintercept = cdq50), color = "red", linetype = "dashed", size = 1) +
     geom_vline(aes(xintercept = cdq75), color = "red", linetype = "dashed", size = 1) +
--- R/esd_table.R
+++ R/esd_table.R
@@ -1,9 +1,9 @@
-esd_table <- function(df, column, cohensd) {
-    esd_table <- df %>%
-      group_by(column) %>%
-      summarise(cdq25 = quantile(df$cd, prob = .25),
-              cdq50 = quantile(df$cd, prob = .50),
-              cdq75 = quantile(df$cd, prob = .75)
-              )
-    esd_table
+esd_table <- function(df, grouping_var, cohensd) {
+  table <- df %>%
+    group_by({{ grouping_var }}) %>%
+    summarise(cdq25 = quantile({{ cohensd }}, prob = .25),
+              cdq50 = quantile({{ cohensd }}, prob = .50),
+              cdq75 = quantile({{ cohensd }}, prob = .75),)
+
+  return(table)
 }
