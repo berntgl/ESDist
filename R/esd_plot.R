2dd42bdb9133145bb3fd2f808f7c9ec2dc797203
Ben Glaser <bernt@glsr.nl>
2022-12-20 12:35:21 +0100

    updated plot layout, added legend

--- R/esd_plot.R
+++ R/esd_plot.R
@@ -10,38 +10,36 @@
 #' @export
 #'
 #' @examples esd_plot(df, es, es_type = "Cohen's d", method = thirds)
-esd_plot <- function(df, es, es_type, method = "quads", bin_width = 0.05) {
+esd_plot <- function(df, es, es_type, method = "quads", bin_width = 0.1) {
   es_col <- df[, deparse(substitute(es))]
   if (method == "quads") {
-    cdq1 <- quantile(es_col, prob = 0.25)
-    cdq1_label <- "25%"
-    cdq2 <- quantile(es_col, prob = 0.50)
-    cdq2_label <- "50%"
-    cdq3 <- quantile(es_col, prob = 0.75)
-    cdq3_label <- "75%"
+    q1 <- quantile(es_col, prob = 0.25)
+    q1_label <- "25th"
+    q2 <- quantile(es_col, prob = 0.50)
+    q2_label <- "50th"
+    q3 <- quantile(es_col, prob = 0.75)
+    q3_label <- "75th"
   } else if (method == "thirds") {
-    cdq1 <- quantile(es_col, prob = 0.1665)
-    cdq1_label <- "16.65%"
-    cdq2 <- quantile(es_col, prob = 0.50)
-    cdq2_label <- "50%"
-    cdq3 <- quantile(es_col, prob = 0.8335)
-    cdq3_label <- "83.35%"
+    q1 <- quantile(es_col, prob = 0.1665)
+    q1_label <- "16.65th"
+    q2 <- quantile(es_col, prob = 0.50)
+    q2_label <- "50th"
+    q3 <- quantile(es_col, prob = 0.8335)
+    q3_label <- "83.35th"
   } else {
     return("Please enter a valid method")
   }
   plot <- ggplot(data = df, aes(es_col)) +
-    geom_histogram(fill = "#0C6170", binwidth = bin_width) +
-    geom_vline(aes(xintercept = cdq1), color = "#E5A4A9", linetype = "dashed", size = 1) +
-    annotate("text", x = (cdq1 + 0.05), y = 2, color = "#E5A4A9", label = cdq1_label, angle = 90) +
-    geom_vline(aes(xintercept = cdq2), color = "#E5A4A9", linetype = "dashed", size = 1) +
-    annotate("text", x = (cdq2 + 0.05), y = 2, color = "#E5A4A9", label = cdq2_label, angle = 90) +
-    geom_vline(aes(xintercept = cdq3), color = "#E5A4A9", linetype = "dashed", size = 1) +
-    annotate("text", x = (cdq3 + 0.05), y = 2, color = "#E5A4A9", label = cdq3_label, angle = 90) +
+    theme_minimal()+
+    geom_histogram(fill = "#355C7D", colour = NA, binwidth = bin_width) +
     scale_x_continuous(breaks = seq(0, 3, 0.5)) +
-    labs(x = es_type, y = "Frequency") +
-    theme(axis.title.x = element_text(face = "bold", size = 20),
-          axis.text.x  = element_text(size = 16),
-          axis.title.y = element_text(face = "bold", size = 20),
-          axis.text.y  = element_text(size = 16))
+    geom_vline(aes(xintercept = q1, color = "q1"), linetype = "dashed", size = 1) +
+    geom_vline(aes(xintercept = q2, color = "q2"), linetype = "dashed", size = 1) +
+    geom_vline(aes(xintercept = q3, color = "q3"), linetype = "dashed", size = 1) +
+    scale_color_manual(name = "Percentiles",
+                       values = c(q1 = "#F8B195", q2 = "#F67280", q3 = "#C06C84"),
+                       labels = c(q1_label, q2_label, q3_label))+
+    theme(legend.position = c(0.9, 0.7), legend.background = element_rect(fill="#dde7f0", color = "#dde7f0")) +
+    labs(x = es_type, y = "Frequency")
   return(plot)
 }
